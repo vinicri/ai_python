5de55dccb323300e0cf37719c8949811a8864bd6
vinicri <vinicri>
2023-12-15 17:12:19 +0000

    notebook

---
+++ source.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+display(spark.sql("SELECT * FROM diamonds LIMIT 10"))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * from diamonds;
+# MAGIC SELECT * from diamonds;
+# MAGIC SELECT * from diamonds;
+# MAGIC SELECT * from diamonds;
+# MAGIC SELECT * from diamonds;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # test
+
+# COMMAND ----------
+
+
